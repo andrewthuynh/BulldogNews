5717643fa2e9f1564eba9e3c9d33c67f13748e7f
Andy <att35663@uga.edu>
2019-06-06 16:13:40 -0400

    Added button with simple axios call

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, Button, View } from "react-native";
+import axios from "axios";
 
 class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Initial Data"
+    };
+    this.getData = this.getData.bind(this);
+  }
+  getData() {
+    let firstMovieTitle;
+    axios.get("https://facebook.github.io/react-native/movies.json").then(function(response) {
+      firstMovieTitle = response.data.movies;
+      console.log(firstMovieTitle);
+    });
 
-    render(){
-        return(
-            <Text>Home</Text>
-        )
-    }
+    this.setState({
+      name: firstMovieTitle
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>{this.state.name}</Text>
+        <Button onPress={this.getData} title="Click Me!" />
+      </View>
+    );
+  }
 }
 
 export default HomeScreen;
