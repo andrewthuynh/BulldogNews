90e35e783586a4d507df83a76c0c1512f1fcf02a
Andrew Huynh <andrewtaylorhuynh@gmail.com>
2019-06-07 23:40:37 -0400

    initial search

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {
+    View,
+    Screen,
+    TextInput,
+    Text,
+    Title,
+    Button,
+    Image,
+    Icon,
+    Row,
+    ScrollView,
+    Heading
+} from '@shoutem/ui';
+import axios from 'axios';
 
 class SearchScreen extends Component {
 
-    render(){
-        return(
-            <Text>Search</Text>
-        )
+    state = {
+        articles: [],
+        search: "",
+    }
+
+    componentDidMount() {
+
+    }
+
+    getArticles = async (tag) => {
+        try {
+            await axios
+                .get(`${baseURL}/api/article/?tag=${tag}`)
+                .then(res => {
+                    this.setState({
+                        articles: res.data
+                    })
+                })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    render() {
+        const { recipes, search } = this.state;
+
+        return (
+            <ScrollView>
+                <View style={styles.container2}>
+                <Heading>Search</Heading>
+                </View>
+                <View style={styles.container}>
+                    <View style={{ margin: 7 }} />
+                    <Row>
+                        <Icon name="search" />
+                        <TextInput
+                            placeholder='Search articles...'
+                            placeholderTextColor="white"
+                            onChangeText={(search) => this.setState({ search })}
+                            style={styles.search}
+                            autoCapitalize="none"
+                        />
+                        <Button
+                            styleName="secondary"
+                            onPress={() => this.getArticles(search)}
+                        >
+                            <Text>SEARCH</Text>
+                        </Button>
+                    </Row>
+                    <View style={{ margin: 20 }} />
+                    {articles.length!=0 && SearchList}
+                    <View style={{ margin: 20 }} />
+                </View>
+            </ScrollView>
+        );
     }
 }
 
 export default SearchScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    search: {
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#FF5722',
+        borderRadius: 15,
+        width: 220
+    },
+    container2: {
+        flex: 1,
+        alignItems: 'center',
+        //backgroundColor: "#F5F5F5"
+      },
+});
