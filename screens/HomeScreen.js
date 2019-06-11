476c7631d87f1ac7f4c7a282d72b18376e6a6f2e
Andy <att35663@uga.edu>
2019-06-11 11:05:09 -0400

    Reformatted code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,36 +3,33 @@ import { StyleSheet, Text, Button, View } from "react-native";
 import axios from "axios";
 
 class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "Initial Data"
-        };
-        this.getData = this.getData.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Initial Data"
+    };
+  }
 
-    getData = async () => {
-        try {
-            await axios
-                .get(`https://facebook.github.io/react-native/movies.json`)
-                .then(res => {
-                    this.setState({
-                        name: res.data.movies[0].title
-                    })
-                })
-        } catch (err) {
-            console.log(err);
-        }
+  getData = async () => {
+    try {
+      await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0c69f9ba9a494e84a33b2749dab3fc5e`).then(res => {
+        this.setState({
+          name: res.data.articles[0].title
+        });
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-render() {
+  render() {
     return (
-        <View>
-            <Text>{this.state.name}</Text>
-            <Button onPress={this.getData} title="Click Me!" />
-        </View>
+      <View>
+        <Text>{this.state.name}</Text>
+        <Button onPress={this.getData} title="Click Me!" />
+      </View>
     );
-}
+  }
 }
 
 export default HomeScreen;
