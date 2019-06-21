f54982b7478fb6bc92457b049c922150203c56bc
Andrew Huynh <andrewtaylorhuynh@gmail.com>
2019-06-20 22:32:49 -0400

    setup categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {
+    View,
+    ScrollView,
+    Heading,
+} from '@shoutem/ui';
 
 class CategoriesScreen extends Component {
 
     render(){
         return(
-            <Text>Categories</Text>
+            <ScrollView>
+            <View style={styles.container}>
+            <Heading>US News</Heading>
+            <Heading>Sports</Heading>
+            <Heading>Fashion</Heading>
+            <Heading>Food</Heading>
+            <Heading>Technology</Heading>
+            <Heading>Music</Heading>
+            </View>
+            </ScrollView>
         )
     }
 }
 
 export default CategoriesScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+  
+    }
+  });
